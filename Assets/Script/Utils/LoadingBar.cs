171c1336250fa2ece013da2bb36dc13c4851a04c
Umesh Lahudkar <99596032+umeshlahudkar@users.noreply.github.com>
2024-02-04 20:25:31 +0530

    loading bar modified

--- Assets/Script/Utils/LoadingBar.cs
+++ Assets/Script/Utils/LoadingBar.cs
@@ -1,14 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadingBar : MonoBehaviour
 {
     [SerializeField] private Transform rotatingObjTran;
-    [SerializeField] private float speed;
+    [SerializeField] private Image RotatingImage;
+    [SerializeField] private AnimationCurve animationCurve;
+
+    [SerializeField] private float rotationSpeed;
+
+    private readonly float imgFillTime = 1.5f;
+    private float currentTime = 0;
+
+    private void OnEnable()
+    {
+        currentTime = 0;
+        rotatingObjTran.rotation = Quaternion.Euler(0, 0, 0);
+    }
 
     private void Update()
     {
-        rotatingObjTran.Rotate(0, 0, -Time.deltaTime * speed);
+        rotatingObjTran.Rotate(0, 0, -Time.deltaTime * rotationSpeed);
+
+        currentTime += Time.deltaTime;
+        RotatingImage.fillAmount = animationCurve.Evaluate( currentTime/imgFillTime );
+        if (currentTime >= imgFillTime)
+        {
+            currentTime = 0.1f;
+        }
     }
 }
